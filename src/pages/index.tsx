10d73a7840a2b4ca14cbd24f3040c8b44fee98ee
Krushna Kanta Rout <129386740+krushnarout@users.noreply.github.com>
2025-01-20 22:38:12 +0530

    fix: clean up and implement search functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,12 @@
-import { ReactNode, useEffect } from 'react';
-import { Redirect } from '@docusaurus/router';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
-
-import styles from './index.module.css';
+import { useEffect } from 'react';
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
-
   // Redirect to Quickstart page
   useEffect(() => {
-    window.location.replace('/docs/quickstart');
+    if (typeof window !== 'undefined') {
+      window.location.replace('/docs/quickstart');
+    }
   }, []);
+
+  return null;
 }
